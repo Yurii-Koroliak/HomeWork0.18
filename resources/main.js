06b6f8edf923b77782ded0918b5cd79d8f2ff9a5
Yurii <j.koroliak@gmail.com>
2023-04-22 19:12:02 +0200

    AJAX

--- resources/main.js
+++ resources/main.js
@@ -4,38 +4,38 @@ const typeSelect = document.getElementById('movie-type');
 const movieList = document.getElementById('movie-list');
 
 form.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const title = titleInput.value;
-  const type = typeSelect.value;
+    const title = titleInput.value;
+    const type = typeSelect.value;
 
-  let apiUrl = 'https://www.omdbapi.com/?s=' + encodeURIComponent(title);
-  if (type) {
-    apiUrl += '&type=' + encodeURIComponent(type);
-  }
-  apiUrl += '&apikey=3bf129a2';
-
-fetch(apiUrl)
-.then(function(response) {
-    if (response.ok) {
-    return response.json();
+    let apiUrl = 'https://www.omdbapi.com/?s=' + encodeURIComponent(title);
+    if (type) {
+        apiUrl += '&type=' + encodeURIComponent(type);
     }
-    throw new Error('Network response was not ok.');
-})
-.then(function(data) {
-    if (data.Response === 'True') {
-        const movies = data.Search;
-        let html = '';
-        for (let i = 0; i < movies.length; i++) {
-            html += '<p>' + movies[i].Title + '</p>';
+    apiUrl += '&apikey=3bf129a2';
+
+    fetch(apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+        return response.json();
         }
-        movieList.innerHTML = html;
-    } else {
-        movieList.innerHTML = '<p>Movie not found!</p>';
-    }
+        throw new Error('Network response was not ok.');
     })
-.catch(function(error) {
-    movieList.innerHTML = '<p>Error searching for movie.</p>';
-});
+    .then(function(data) {
+        if (data.Response === 'True') {
+            const movies = data.Search;
+            let html = '';
+            for (let i = 0; i < movies.length; i++) {
+                html += '<p>' + movies[i].Title + '</p>';
+            }
+            movieList.innerHTML = html;
+        } else {
+            movieList.innerHTML = '<p>Movie not found!</p>';
+        }
+        })
+    .catch(function(error) {
+        movieList.innerHTML = '<p>Error searching for movie.</p>';
+    });
 });
 
