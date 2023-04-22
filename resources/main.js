cb545c103c34114e8e25bd30cf662fbf5a6219e4
Yurii <j.koroliak@gmail.com>
2023-04-22 18:46:49 +0200

    AJAX

---
+++ resources/main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('movie-form');
+const titleInput = document.getElementById('movie-title');
+const typeSelect = document.getElementById('movie-type');
+const movieList = document.getElementById('movie-list');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const title = titleInput.value;
+  const type = typeSelect.value;
+
+  let apiUrl = 'http://www.omdbapi.com/?s=' + encodeURIComponent(title);
+  if (type) {
+    apiUrl += '&type=' + encodeURIComponent(type);
+  }
+  apiUrl += '&apikey=3bf129a2';
+
+fetch(apiUrl)
+.then(function(response) {
+    if (response.ok) {
+    return response.json();
+    }
+    throw new Error('Network response was not ok.');
+})
+.then(function(data) {
+    if (data.Response === 'True') {
+        const movies = data.Search;
+        let html = '';
+        for (let i = 0; i < movies.length; i++) {
+            html += '<p>' + movies[i].Title + '</p>';
+        }
+        movieList.innerHTML = html;
+    } else {
+        movieList.innerHTML = '<p>Movie not found!</p>';
+    }
+    })
+    .catch(function(error) {
+      movieList.innerHTML = '<p>Error searching for movie.</p>';
+    });
+});
+
